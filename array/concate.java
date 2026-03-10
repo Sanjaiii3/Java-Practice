450a56a269bd618ffb9924e3ad14599adfc3ce00
Sanjai <your-email@example.com>
2026-03-10 14:15:32 +0530

    first commit

--- array/concate.java
+++ array/concate.java
@@ -1,5 +1,42 @@
 package array;
 
+import java.util.Scanner;
+
 public class concate {
-    
+
+    public static int[] getConcatenation(int[] nums) {
+        int ans[] = new int[nums.length * 2];
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i + nums.length] = nums[i];
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // input array size
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        // input array elements
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int[] result = getConcatenation(nums);
+
+        // print result
+        System.out.println("Concatenated array:");
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
 }
+
