2ebd477707ed84850c7afdc50d5511e9bd521ea1
Sanjai <sanjaipushparaj@gmail.com>
2026-08-05 01:17:24 +0530

    Initial Commit

---
+++ HashSet/ArrayUnion.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class ArrayUnion {
+
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int ans[] = new int[a.length + b.length];
+        int ind = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            ans[ind++] = a[i];
+        }
+
+        for (int j = 0; j < b.length; j++) {
+            ans[ind++] = b[j];
+        }
+
+        Arrays.sort(ans);
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i : ans) {
+            if (!set.contains(i)) {
+                list.add(i);
+                set.add(i);
+            }
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of first array: ");
+        int n = sc.nextInt();
+
+        int[] a = new int[n];
+        System.out.println("Enter elements of first sorted array:");
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter size of second array: ");
+        int m = sc.nextInt();
+
+        int[] b = new int[m];
+        System.out.println("Enter elements of second sorted array:");
+        for (int i = 0; i < m; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        ArrayList<Integer> ans = findUnion(a, b);
+
+        System.out.println("Union of the arrays:");
+        for (int x : ans) {
+            System.out.print(x + " ");
+        }
+
+        sc.close();
+    }
+}
