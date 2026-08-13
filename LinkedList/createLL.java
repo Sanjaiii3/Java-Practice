a1679a547f058951eb280ac8aae5a2939a9d9afd
Sanjai <sanjaipushparaj@gmail.com>
2026-08-14 00:29:42 +0530

    initial commit

--- LinkedList/createLL.java
+++ LinkedList/createLL.java
@@ -16,6 +16,9 @@ public class createLL{
             temp=temp.next;
         }
         System.out.print("null");
+        System.out.println();
+        int lengthOfLL=length(head);
+        System.out.print("length of linked list:"+lengthOfLL);
     }
     private static Node arraytolist(int []arr){
         Node head=new Node(arr[0]);
@@ -26,5 +29,14 @@ public class createLL{
             mover=temp;
         }
         return head;
+    }
+    public static int length(Node head){
+        int count=0;
+        Node temp=head;
+        while(temp!=null){
+            temp=temp.next;
+            count++;
+        }
+        return count;
     }
 }
